c807b288dbadd87a77d033d0065d4301319a96b0
mkripsevics <martins.kripsevics@gmail.com>
2024-02-25 12:39:56 +0200

    Commit rename

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class test1 {
+public class Test1 {
     private WebDriver driver;
 
     @Before
