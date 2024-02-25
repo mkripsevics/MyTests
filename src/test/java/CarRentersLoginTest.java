8a13ce482e13664e794745ae63e69cd2ea103c44
mkripsevics <martins.kripsevics@gmail.com>
2024-02-25 12:38:23 +0200

    Commit rename

--- src/test/java/CarRentersLoginTest.java
+++ src/test/java/CarRentersLoginTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CarrentersLoginTest {
+public class CarRentersLoginTest {
     private WebDriver driver;
 
     @Before
